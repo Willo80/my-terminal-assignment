01c30e37bd33fbd79ae6bc7203c8bc75f272b5c0
Carol <carol@yeselearning.com.au>
2022-10-19 13:32:11 +1100

    syntax fixes

--- functs.py
+++
@@ -1,7 +0,0 @@
-# imported from pypi.org
-
-from termcolor import colored, cprint
-
-text = colored("Hello, World!", "red", attrs=["reverse", "blink"])
-print(text)
-cprint("Hello, World!", "green", "on_red")
