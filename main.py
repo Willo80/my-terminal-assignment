ce6954854669611d7bd2d8cbee794cc3b77b8fc7
Carol <carol@yeselearning.com.au>
2022-10-19 11:43:49 +1100

    included except value error on number input

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ from Question import Question
 def main():
     """Wrapped entire code - user has option to restart at end of quiz"""
 
-    print("\nLet's get tracking")      
+    print("\nLet's get tracking")
 
-    # Input full name - surname used for future ref on app updates, but not in this first instance.
+    # Must input full name as surname used for future on app update, but not in this first instance.
     while True:
         try:
             name = input("\nPlease type your name: ").title().strip()
@@ -29,7 +29,14 @@ def main():
     play = input ("\nType Yes for a quick multiple-choice game or NO to enter your reading: ").lower()
     if play != "yes":
         # User completes the BGL pathway and skips game
-        glucose_reading = float(input("\nEnter the number reading from your finger prick test: "))
+
+        while True:
+            try:
+                glucose_reading = float(input("\nEnter the number reading from your finger prick test: "))
+                break
+            except ValueError:
+                cprint("That is not a number, please type the number reading from your finger prick device", "red")
+
         fasting = input("\nType in F if you were fasting or NF if you were not fastng: ").capitalize()
 
         if glucose_reading < 8 and fasting == "F":
@@ -44,8 +51,8 @@ def main():
         elif glucose_reading < 8 and fasting != "F":
             print("\nYour reading is low for having just eaten, a second fasting reading may be more accurate")
 
-        # The date and time of input is recorded for future reference
-        cprint(f"\nThe time and date of your input is {datetime.datetime.now()}", "green")
+            # The date and time of input is recorded for future reference
+            cprint(f"\nThe time and date of your input is {datetime.datetime.now()}", "green")
 
         exit()
 
@@ -53,51 +60,52 @@ def main():
     # Quiz begins here
     elif play == "yes":
         cprint("\nExcellent! There are 4 questions, try and score 100%", "green")
+        
         # User can play or quit at this point if they don't want to continue
         play = input("Okay, if you are ready type Yes, or No to quit: ").lower()
         if play != "yes":
             quit()
 
 
-    # Q's' = my own Class function, allows updates in future
-    ask_question = [
-    "\nWhich of these will control your blood sugar better?\n(a) Chocolate\n(b) Banana\n(c) Pizza\n\n",
-    "\nLook out for food with a ___ Glycemic Index: high or low?\n(a) High\n(b) Low\n\n",
-    "\nDaily exercise will help manage your diabetes:\n(a) True\n(b) False\n\n",
-    "\nWhen should you do a finger prick reading?\n\n(a) After eating\n(b) Before eating\n\n",
-    ]
+        # Q's' = my own Class function, allows updates in future
+        ask_question = [
+        "\nWhich of these will control your blood sugar better?\n(a) Chocolate\n(b) Banana\n(c) Pizza\n\n",
+        "\nLook out for food with a ___ Glycemic Index: high or low?\n(a) High\n(b) Low\n\n",
+        "\nDaily exercise will help manage your diabetes:\n(a) True\n(b) False\n\n",
+        "\nWhen should you do a finger prick reading?\n\n(a) After eating\n(b) Before eating\n\n",
+        ]
 
-    questions = [
-        Question(ask_question[0], "b"),
-        Question(ask_question[1], "b"),
-        Question(ask_question[2], "a"),
-        Question(ask_question[3], "b"),
-    ]
+        questions = [
+            Question(ask_question[0], "b"),
+            Question(ask_question[1], "b"),
+            Question(ask_question[2], "a"),
+            Question(ask_question[3], "b"),
+        ]
 
-    def begin_quiz(questions):
-        """def to ask user selection of questions"""
-        score = 0
-        for question in questions:
-            answer = input(question.ask)
-            if answer == question.answer:
-                cprint("\nCorrect!", "green")
-                score +=25
-            else:
-                cprint("\nThat is incorrect", "red")
+        def begin_quiz(questions):
+            """def to ask user selection of questions"""
+            score = 0
+            for question in questions:
+                answer = input(question.ask)
+                if answer == question.answer:
+                    cprint("\nCorrect!", "green")
+                    score +=25
+                else:
+                    cprint("\nThat is incorrect", "red")
 
-        cprint("\nYou scored " + str(score) + "%"
-        + "\n\nContinue learning more about diabetes for better control over your wellness", "green")
-        if score <= 50:
-            cprint("The more you know the better, feel free to try again?", "red")
-        elif score > 51:
-            cprint("Your knowledge is improving!", "green")
+            cprint("\nYou scored " + str(score) + "%"
+            + "\n\nContinue learning more about diabetes for better control over your wellness", "green")
+            if score <= 50:
+                cprint("The more you know the better, feel free to try again?", "red")
+            elif score > 51:
+                cprint("Your knowledge is improving!", "green")
 
-    begin_quiz(questions)
-    # Option to go back and enter BGL
-    restart=input("\nWould you like to return to the start and add your BGL?: ").lower()
-    if restart == "yes":
-        main()
-    else:
-        exit()
+        begin_quiz(questions)
+        # Option to go back and enter BGL
+        restart=input("\nWould you like to return to the start and add your BGL?: ").lower()
+        if restart == "yes":
+            main()
+        else:
+            exit()
     
 main()
